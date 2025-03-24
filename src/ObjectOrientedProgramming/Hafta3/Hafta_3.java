794ae056512dbe7ffd72417041c1b847ab207db6
Tayyip ÇİTİL <compengtayyip@gmail.com>
2025-03-24 23:36:05 +0300

    update code

--- src/ObjectOrientedProgramming/Hafta3/Hafta_3.java
+++ src/ObjectOrientedProgramming/Hafta3/Hafta_3.java
@@ -5,12 +5,12 @@ public class Hafta_3 {
         String name1=new String("Hello");
         String name2="hello";
         System.out.println(name1);
-        Double num1= new Double(123);
+        //Double num1= new Double(123);
         double num2=12.1;
-        System.out.println(num1);
-        Integer n1=new Integer(5);
+        //System.out.println(num1);
+        //Integer n1=new Integer(5);
         int n2=6;
-        System.out.println(n1);
+        //System.out.println(n1);
 
 
     Sinif ogr1=new Sinif();
